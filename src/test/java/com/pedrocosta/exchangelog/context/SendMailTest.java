045ce238bd48869e61aebc6916824854649afcc9
phmdacosta <ph.mcosta@hotmail.com>
2021-05-25 18:00:56 +0100

    implementing mail sender

---
+++ src/test/java/com/pedrocosta/exchangelog/context/SendMailTest.java
@@ -0,0 +1,32 @@
+package com.pedrocosta.exchangelog.context;
+
+import com.pedrocosta.exchangelog.ExchangelogApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * @author Pedro H M da Costa
+ * @version 1.0
+ */
+@SpringBootTest(classes = ExchangelogApplication.class)
+@TestPropertySource(locations = "classpath:application.properties")
+public class SendMailTest {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Test
+    public void sendSimpleMessage() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("exchlog-test@gmail.com");
+        message.setTo("exchlog-test@gmail.com");
+        message.setSubject("Mail Sender Test");
+        message.setText("This is a e-mail sending test.");
+        emailSender.send(message);
+    }
+}
