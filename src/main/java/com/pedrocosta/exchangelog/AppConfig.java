d7dfdda1a94c4e53c363f273917466fc07472bf0
Pedro da Costa <ph.mcosta@hotmail.com>
2021-05-25 11:38:55 +0100

    Fixing message bean

--- src/main/java/com/pedrocosta/exchangelog/AppConfig.java
+++ src/main/java/com/pedrocosta/exchangelog/AppConfig.java
@@ -2,9 +2,11 @@ package com.pedrocosta.exchangelog;
 
 import java.time.Duration;
 
+import com.pedrocosta.exchangelog.utils.MessageProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -16,5 +18,15 @@ public class AppConfig {
 				.setConnectTimeout(Duration.ofMillis(3000))
 				.setReadTimeout(Duration.ofMillis(3000))
 				.build();
+	}
+
+	@Bean
+	public static MessageProperties messageProperties() {
+		var source = new ResourceBundleMessageSource();
+		source.setBasenames("label/messages");
+		source.setUseCodeAsDefaultMessage(true);
+		MessageProperties properties = new MessageProperties();
+		properties.setSource(source);
+		return properties;
 	}
 }
