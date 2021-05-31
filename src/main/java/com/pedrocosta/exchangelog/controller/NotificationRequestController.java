a8707ab37a9bf06f7361e55e3e3e2d35a3c07eeb
phmdacosta <ph.mcosta@hotmail.com>
2021-05-31 07:47:20 +0100

    Developing notification request CRUD

---
+++ src/main/java/com/pedrocosta/exchangelog/controller/NotificationRequestController.java
@@ -0,0 +1,30 @@
+package com.pedrocosta.exchangelog.controller;
+
+import com.pedrocosta.exchangelog.services.ServiceFactory;
+import com.pedrocosta.exchangelog.services.WebService;
+import com.pedrocosta.exchangelog.utils.GsonUtils;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Pedro H M da Costa
+ * @version 1.0
+ */
+@Controller
+public class NotificationRequestController {
+
+    private ServiceFactory serviceFactory;
+    private GsonUtils gsonUtils;
+
+    public NotificationRequestController(ServiceFactory serviceFactory, GsonUtils gsonUtils) {
+        this.serviceFactory = serviceFactory;
+        this.gsonUtils = gsonUtils;
+    }
+
+    @RequestMapping(value = "/notif-request/save", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String save(@RequestBody String notReqJson) {
+        return null;
+    }
+}
