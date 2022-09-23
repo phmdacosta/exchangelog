2b068a3de7d5f6931300b2057706ba7b9b6bb3df
phmdacosta <ph.mcosta@hotmail.com>
2022-09-23 14:54:33 +0100

    Fixing Notification REST API
    Creating a interface for service layer

--- user-manager/src/main/java/com/pedrocosta/exchangelog/auth/api/NotificationRestService.java
+++ user-manager/src/main/java/com/pedrocosta/exchangelog/auth/api/NotificationRestService.java
@@ -1,18 +1,15 @@
 package com.pedrocosta.exchangelog.auth.api;
 
+import com.pedrocosta.exchangelog.api.GetServiceRequest;
+import com.pedrocosta.exchangelog.api.PostServiceRequest;
 import com.pedrocosta.exchangelog.api.RestServiceRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Class to connect to external notification service.
+ */
 @Service
-public class NotificationRestService extends RestServiceRequest<String> {
+public interface NotificationRestService extends GetServiceRequest<String>, PostServiceRequest<String> {
 
-    public NotificationRestService(RestTemplate restTemplate) {
-        super(restTemplate);
-    }
-
-    @Override
-    public void load() {
-        getMapProp().put(REQUEST_URL_REF, "http://localhost:8089/");
-    }
 }
---
+++ user-manager/src/main/java/com/pedrocosta/exchangelog/auth/api/NotificationRestServiceImpl.java
@@ -0,0 +1,18 @@
+package com.pedrocosta.exchangelog.auth.api;
+
+import com.pedrocosta.exchangelog.api.RestServiceRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationRestServiceImpl extends RestServiceRequest<String> implements NotificationRestService{
+
+    public NotificationRestServiceImpl(RestTemplate restTemplate) {
+        super(restTemplate);
+    }
+
+    @Override
+    public void load() {
+        getMapProp().put(REQUEST_URL_REF, "http://localhost:8089/");
+    }
+}
