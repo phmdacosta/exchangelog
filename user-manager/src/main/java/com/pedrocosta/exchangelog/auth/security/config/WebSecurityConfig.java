86dd751471a57f71d55b5ed1723f752fe730670c
phmdacosta <ph.mcosta@hotmail.com>
2022-10-14 15:24:05 +0100

    Custom login form
    Developing flexibility for custom login form

--- user-manager/src/main/java/com/pedrocosta/exchangelog/auth/security/config/WebSecurityConfig.java
+++ user-manager/src/main/java/com/pedrocosta/exchangelog/auth/security/config/WebSecurityConfig.java
@@ -1,24 +1,18 @@
 package com.pedrocosta.exchangelog.auth.security.config;
 
-import com.pedrocosta.exchangelog.auth.permission.Permission;
-import com.pedrocosta.exchangelog.auth.role.Role;
 import com.pedrocosta.exchangelog.auth.role.RoleService;
 import com.pedrocosta.exchangelog.auth.user.UserService;
 import com.pedrocosta.exchangelog.auth.utils.Route;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collection;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -48,11 +42,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(Route.API + Route.REGISTRATION + "/**").permitAll()
                 .anyRequest()
                 .access("@roleAuthorityValidator.accept(authentication,request)")
-                .and().formLogin();
+                .and().formLogin(formLoginCustomizer());
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    protected Customizer<FormLoginConfigurer<HttpSecurity>> formLoginCustomizer() {
+        return httpSecurityFormLoginConfigurer -> {};
     }
 }
