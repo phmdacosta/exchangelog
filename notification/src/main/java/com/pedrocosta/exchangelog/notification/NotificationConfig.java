243f8402091612f3b71430167e06f6bd0004b893
phmdacosta <ph.mcosta@hotmail.com>
2022-11-23 18:03:50 +0000

    Including batch tasks into notification app

--- notification/src/main/java/com/pedrocosta/exchangelog/notification/NotificationConfig.java
+++ notification/src/main/java/com/pedrocosta/exchangelog/notification/NotificationConfig.java
@@ -1,8 +1,11 @@
 package com.pedrocosta.exchangelog.notification;
 
 import com.pedrocosta.exchangelog.BaseConfig;
+import com.pedrocosta.exchangelog.batch.BatchSchedulerConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-public class NotificationConfig extends BaseConfig {
+@Import({BaseConfig.class, BatchSchedulerConfig.class})
+public class NotificationConfig {
 }
