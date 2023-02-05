b3d08b1a692d01fc9eee2f8e712e0d9c2d76473a
phmdacosta <ph.mcosta@hotmail.com>
2023-02-05 19:35:33 +0000

    NotificationSenderFactory improvement
    Changing the creation of a sender in the factory to be able to create senders from different packages

--- notification/src/main/java/com/pedrocosta/exchangelog/notification/sender/NotificationSenderFactory.java
+++ notification/src/main/java/com/pedrocosta/exchangelog/notification/sender/NotificationSenderFactory.java
@@ -1,11 +1,16 @@
 package com.pedrocosta.exchangelog.notification.sender;
 
+import com.pedrocosta.exchangelog.CoreService;
 import com.pedrocosta.exchangelog.notification.Mean;
 import com.pedrocosta.exchangelog.notification.Notification;
+import com.pedrocosta.springutils.PackageUtils;
 import com.pedrocosta.springutils.output.Log;
+import com.pedrocosta.springutils.output.Messages;
 import com.sun.istack.NotNull;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class NotificationSenderFactory {
@@ -30,15 +35,23 @@ public class NotificationSenderFactory {
         return create(context, mean);
     }
 
-    public static NotificationSender create(@NotNull ApplicationContext context,
-                                            @NotNull Mean mean) {
+    public NotificationSender create(@NotNull final ApplicationContext context,
+                                            @NotNull final Mean mean) {
         String className = mean.capitalizeName().concat(SUFFIX);
+
         try {
-            Class<?> senderClass =  Class.forName(NotificationSenderFactory.class.getPackageName() + "." + className);
-            return (NotificationSender) context.getBean(senderClass);
+            // Look for all subpackages into service
+            Package projectPackage = PackageUtils.getProjectPackage(context);
+            List<Package> subPackages = PackageUtils.getSubPackages(projectPackage.getName());
+
+            for (Package pack : subPackages) {
+                Class<?> senderClass = Class.forName(pack.getName() + "." + className);
+                return (NotificationSender) context.getBean(senderClass);
+            }
         } catch (ClassNotFoundException e) {
-            Log.error(context, e);
+            Log.error(this, e);
         }
+
         return null;
     }
 }
