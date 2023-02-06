5658bc94a60300e9280501fa718c2df740118c07
phmdacosta <ph.mcosta@hotmail.com>
2023-02-06 10:52:02 +0000

    Notification sender factory improvement
    Using new utility methods to search for senders

--- notification/src/main/java/com/pedrocosta/exchangelog/notification/sender/NotificationSenderFactory.java
+++ notification/src/main/java/com/pedrocosta/exchangelog/notification/sender/NotificationSenderFactory.java
@@ -3,14 +3,20 @@ package com.pedrocosta.exchangelog.notification.sender;
 import com.pedrocosta.exchangelog.CoreService;
 import com.pedrocosta.exchangelog.notification.Mean;
 import com.pedrocosta.exchangelog.notification.Notification;
+import com.pedrocosta.springutils.ClassFinder;
 import com.pedrocosta.springutils.PackageUtils;
 import com.pedrocosta.springutils.output.Log;
 import com.pedrocosta.springutils.output.Messages;
 import com.sun.istack.NotNull;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AssignableTypeFilter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 @Component
 public class NotificationSenderFactory {
@@ -37,15 +43,12 @@ public class NotificationSenderFactory {
 
     public NotificationSender create(@NotNull final ApplicationContext context,
                                             @NotNull final Mean mean) {
-        String className = mean.capitalizeName().concat(SUFFIX);
-
         try {
-            // Look for all subpackages into service
-            Package projectPackage = PackageUtils.getProjectPackage(context);
-            List<Package> subPackages = PackageUtils.getSubPackages(projectPackage.getName());
-
-            for (Package pack : subPackages) {
-                Class<?> senderClass = Class.forName(pack.getName() + "." + className);
+            List<Class<?>> senderClasses = ClassFinder.findAllByAssignable(context, NotificationSender.class);
+            Class<?> senderClass = senderClasses.stream().filter(
+                    clazz -> clazz.getSimpleName().startsWith(mean.capitalizeName()))
+                    .findFirst().orElse(null);
+            if (senderClass != null) {
                 return (NotificationSender) context.getBean(senderClass);
             }
         } catch (ClassNotFoundException e) {
